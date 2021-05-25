abd9c118f1836419e80837d819fd63338bc784e4
Janet Woods <janetnwoods@gmail.com>
2021-05-25 10:27:44 -0400

    moved products to product.service

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,7 +3,7 @@ import {ProductsComponent} from '../products/products.component'
 import {FormsModule} from '@angular/forms';
 import { IProduct } from '../products/product';
 import {StarComponent} from '../star/star.component';
-
+import {ProductService} from '../products/product.service';
 @Component({
   selector: 'product-list',
   templateUrl: './product-list.component.html',
@@ -11,6 +11,7 @@ import {StarComponent} from '../star/star.component';
 })
 export class ProductListComponent implements OnInit {
   onNotify(message: string):void{}
+
   pageTitle = "Product List";
     imageWidth = 50;
     imageMargin = 2;
@@ -38,61 +39,13 @@ export class ProductListComponent implements OnInit {
     filteredProducts: IProduct[] = [];
 
     products: IProduct[]= [
-      {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2021",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "assets/images/leaf_rake.png"
-      },
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2021",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png"
-      },
-      {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2021",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "assets/images/hammer.png"
-      },
-      {
-        "productId": 8,
-        "productName": "Saw",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2021",
-        "description": "15-inch steel blade hand saw",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "assets/images/saw.png"
-      },
-      {
-        "productId": 10,
-        "productName": "Video Game Controller",
-        "productCode": "GMG-0042",
-        "releaseDate": "October 15, 2020",
-        "description": "Standard two-button video game controller",
-        "price": 35.95,
-        "starRating": 4.6,
-        "imageUrl": "assets/images/xbox-controller.png"
-      }
+
     ];
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.listFilter='';
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
   performFilter(filterBy: string): IProduct[]{
